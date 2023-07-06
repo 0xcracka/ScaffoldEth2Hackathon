9e999a264f540a2b0888c655ad31d347a8e0348f
cracka <skylor808@gmail.com>
2023-07-06 09:25:47 -1000

    changed contracts

--- packages/hardhat/contracts/EscrowFactory.sol
+++ packages/hardhat/contracts/EscrowFactory.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "./Escrow.sol";
 import "./IEscrow.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
 
 contract EscrowFactory {
     event EscrowCreated(address indexed escrow, address indexed arbiter, address indexed beneficiary);
@@ -10,18 +11,24 @@ contract EscrowFactory {
     mapping(address => address[]) public userEscrows;
     address[] public allEscrows;
 
-    function createEscrow(address arbiter, address beneficiary) external payable returns (address) {
+    function createEscrow(address arbiter, address beneficiary, bytes32 salt) external payable returns (address) {
         require(msg.value > 0, "EscrowFactory: Must provide value to create Escrow");
 
-        // Pass the msg.sender as the funder when creating a new instance of Escrow
-        Escrow escrow = new Escrow{value: msg.value}(arbiter, beneficiary, msg.sender);
-        
-        userEscrows[msg.sender].push(address(escrow));
-        allEscrows.push(address(escrow));
+        // Define the implementation contract for the escrow
+        Escrow escrowImplementation = new Escrow(arbiter, beneficiary, msg.sender);
 
-        emit EscrowCreated(address(escrow), arbiter, beneficiary);
-        return address(escrow);
-    }
+        // Compute the address for the new escrow contract with the given salt
+        address escrowAddress = Clones.predictDeterministicAddress(address(escrowImplementation), salt);
+
+        // Create the new escrow contract using CREATE2
+        Clones.cloneDeterministic(address(escrowImplementation), salt);
+
+        userEscrows[msg.sender].push(escrowAddress);
+        allEscrows.push(escrowAddress);
+
+        emit EscrowCreated(escrowAddress, arbiter, beneficiary);
+        return escrowAddress;
+}
     function getEscrowStatus(address escrowAddress) external view returns (string memory) {
         IEscrow escrow = IEscrow(escrowAddress);
         return escrow.getStatus();
